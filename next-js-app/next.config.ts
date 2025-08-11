62d4b483dd75d79cb0917f322c3fc953c583fe08
James <galavantinggeckoguy@gmail.com>
2025-08-11 11:11:36 -0700

    Test

--- next-js-app/next.config.ts
+++ next-js-app/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/gitJamoo.github.io",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
