c2c08c14b27e2ac0f8ce196b7451f39247c89adf
James <galavantinggeckoguy@gmail.com>
2025-09-16 10:46:29 -0700

    OG iamge

--- next-js-app/src/app/layout.tsx
+++ next-js-app/src/app/layout.tsx
@@ -15,7 +15,23 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "James Smith Portfolio",
-  description: "James Smith Portfolio",
+  description: "James Smith Wormhole Portfolio",
+  openGraph: {
+    title: "James M. Smith - Wormhole Portfolio",
+    description: "Machine Learning Engineer & Software Developer",
+    url: "https://j-m-s.dev",
+    siteName: "James M. Smith",
+    images: [
+      {
+        url: "/image-assets/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "James M. Smith Portfolio OG Image",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
