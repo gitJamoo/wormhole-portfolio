ae0c3db470393ef425bbc23ad8cd67f074f107bb
James <galavantinggeckoguy@gmail.com>
2026-01-22 14:40:47 -0800

    Added redirects

--- next-js-app/next.config.ts
+++ next-js-app/next.config.ts
@@ -4,6 +4,30 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/gh",
+        destination: "https://github.com/gitJamoo",
+        permanent: false,
+      },
+      {
+        source: "/github",
+        destination: "https://github.com/gitJamoo",
+        permanent: false,
+      },
+      {
+        source: "/in",
+        destination: "https://www.linkedin.com/in/james-m-smith1",
+        permanent: false,
+      },
+      {
+        source: "/linkedin",
+        destination: "https://www.linkedin.com/in/james-m-smith1",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
